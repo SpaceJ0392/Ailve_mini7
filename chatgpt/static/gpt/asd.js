56e9ae3fc04471ca5bd119ff95dfd9983ba44da9
park <luke7574@naver.com>
2024-06-12 17:31:39 +0900

    chatbot_final

--- chatgpt/static/gpt/asd.js
+++ chatgpt/static/gpt/asd.js
@@ -36,34 +36,21 @@ $(document).ready(function(){
         }
     }
 
-    const toggleButtonState = function() {
-        if ($('#question').val().trim() === '') {
-            $("#text-button").prop("disabled", true);
-        } else {
-            $("#text-button").prop("disabled", false);
-        }
-    }
-    
-    // 입력란 내용이 변경될 때 버튼 상태 업데이트
-    $("#question").on('input', toggleButtonState);
-    
-    // 초기 버튼 상태 설정
-    toggleButtonState();
-
     var is_valid = 1
-    // 버튼 클릭 시
-    $("#text-button").click(function(event){
-        if (is_valid === 1) {
+
+    if (is_valid == 1) {
+        // 버튼 클릭 시
+        $("#text-button").click(function(event){
+            
             is_valid = 0;
             event.stopPropagation();
             const now = formatDate(new Date());
 
             let query = {type : 'query', data : $("#question").val()}; // textarea에 입력된 데이터 가져오기
             console.log(query)
-            $("#question").val('');
-            addDiv(query);
             
-            initTextareaHeight()
+            addDiv(query);
+            initTextareaHeight(); // 새로운 div가 추가될 때마다 textarea의 높이를 조절
 
             $("#text-button").prop("disabled", true);
 
@@ -76,28 +63,24 @@ $(document).ready(function(){
                 beforeSend: (xhr) => xhr.setRequestHeader("X-CSRFToken", csrfToken), // CSRF 토큰을 헤더에 포함
                 success: (data) => {
                     addDiv(data)
-                    window.scrollTo({left:0, top:document.body.scrollHeight, behavior:'smooth'});
-                    toggleButtonState();
+                    $("#text-button").prop("disabled", false);
                     is_valid = 1;
                     },
                 error: (error) => console.error("Failed to send POST request:", error)
             });
-        }
-    });
+                    
+            $("#question").text('');
+        });
+    }
 
     // 엔터 키 입력 시
     $("#question").on('keydown', function(event){
         if (event.shiftKey) {
                 if(event.keyCode == 13) {
+                    
             }
-        } else if (event.keyCode == 13) {
-            event.preventDefault()
-            if ($('#question').val() !== '' && is_valid === 1 ) {
-                event.preventDefault()
-                $("#text-button").click();
-            }
-        } else if (event.keyCode == 13 && is_valid === 0) {
-            event.preventDefault()
+        } else if (event.keyCode == 13 && is_valid == 1) {
+            $("#text-button").click();
         }
     });
 
@@ -111,30 +94,26 @@ $(document).ready(function(){
         div.style.height = newHeight + 'px'; // 높이를 설정
         textarea.style.height = newHeight + 'px'; // textarea의 높이 설정
     }
-    const defaultHeight = $('#question').css('height');
-    console.log(defaultHeight)
-    // var defaultHeight = textarea.style.height;
 
     function initTextareaHeight() {
         var textarea = document.getElementById('question');
         var div = document.getElementById('question-wrapper');
-        
-        // textarea 내용을 빈 문자열로 설정하여 높이를 0으로 만듭니다.
-        textarea.value = '';
-        // 스크롤 높이를 측정하여 textarea와 wrapper의 높이로 적용합니다.
-        // var newHeight = 50;
-        textarea.style.height = defaultHeight;
-        div.style.height = defaultHeight;
-    
-        adjustTextareaHeight(); // textarea의 높이 자동 조정
+        textarea.style.height = 'auto';
+        console.log(textarea.style.height)
+
+        var newHeight = textarea.scrollHeight; // 입력된 내용의 높이를 가져옴
+        // div.style.height = newHeight + 'px'; // 높이를 설정
+        textarea.style.height = newHeight + 'px'; // textarea의 높이 설정
     }
 
+    // 페이지가 로드될 때와 입력창의 내용이 변경될 때마다 함수를 호출하여 높이를 조정
+    // window.onload = function() {
+    //     adjustTextareaHeight(); // 페이지 로드될 때 호출
+    //     document.getElementById('question').addEventListener('input', adjustTextareaHeight); // 입력창 내용이 변경될 때마다 호출
+    // };
+    $(window).on("load", function() {
+        adjustTextareaHeight(); // 페이지 로드될 때 호출
+    });
     // 입력창의 내용이 변경될 때마다 textarea의 높이를 조정
     $("#question").on("input", adjustTextareaHeight);
 });
-
-// document.addEventListener("DOMContentLoaded", function() {
-//     // main 요소의 높이를 가져와서 CSS 변수로 설정합니다.
-//     const mainHeight = document.getElementById('main').offsetHeight;
-//     document.documentElement.style.setProperty('--main-height', mainHeight + 'px');
-// });
